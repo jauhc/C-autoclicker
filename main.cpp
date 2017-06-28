f7893db8d921a1be200063f3eb7d76cec7b6b5b9
yaho <paperslove420@gmail.com>
2017-06-29 00:19:22 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+#include <Windows.h>
+
+#define HITKEY VK_XBUTTON2
+#define ENDKEY VK_SHIFT
+
+void clickFunction(INPUT* input)
+{
+	if (!GetAsyncKeyState(ENDKEY) && GetAsyncKeyState(HITKEY))
+	{
+		input->mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTDOWN);
+		SendInput(1, input, sizeof(INPUT));
+		Sleep(17);
+
+		input->mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTUP);
+		SendInput(1, input, sizeof(INPUT));
+		Sleep(17);
+	}
+	if (GetAsyncKeyState(ENDKEY))
+		exit(0);
+}
+
+
+void main()
+{
+	INPUT input;
+	input.type = INPUT_MOUSE;
+	input.mi.dx = 0;
+	input.mi.dy = 0;
+	input.mi.mouseData = 0;
+	input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE;
+	input.mi.time = 0;
+	input.mi.dwExtraInfo = 0;
+	printf("%s\n", "[clickenings intensifies]\n");
+	while (1)
+		clickFunction(&input);
+}
